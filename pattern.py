c0d47c4e1be2ec4c8569a26403309ddd450f2224
AlexeySorokin <alexey.sorokin@list.ru>
2020-11-27 08:45:59 +0300

    feat: keep_source in Pattern class

--- pattern.py
+++ pattern.py
@@ -3,14 +3,19 @@ import lcs_naive
 
 class Pattern:
 
-    def __init__(self, prefix_to_remove, prefix_to_add, suffix_to_remove, suffix_to_add):
+    def __init__(self, prefix_to_remove, prefix_to_add, suffix_to_remove, suffix_to_add,
+                 keep_source=True):
+        self.keep_source = keep_source
         self.prefix_to_remove = prefix_to_remove
         self.prefix_to_add = prefix_to_add
         self.suffix_to_remove = suffix_to_remove
         self.suffix_to_add = suffix_to_add
+        if not self.keep_source:
+            self.prefix_to_remove = "." * len(self.prefix_to_remove)
+            self.suffix_to_remove = "." * len(self.suffix_to_remove)
 
     def apply(self, s):
-        if s.startswith(self.prefix_to_remove) and s.endswith(self.suffix_to_remove):
+        if s.startswith(self.prefix_to_remove) and s.endswith(self.suffix_to_remove) or not self.keep_source:
             if len(self.prefix_to_remove) + len(self.suffix_to_remove) <= len(s):
                 stem = s[len(self.prefix_to_remove):(len(s)-len(self.suffix_to_remove))]
                 return self.prefix_to_add + stem + self.suffix_to_add
